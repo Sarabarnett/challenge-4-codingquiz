89d8e68ca07a1fe9d760206b9a617a8586401a2a
SBarn11 <sarabarnett1@outlook.com>
2021-11-26 18:12:06 -0700

    redo wrong and right variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,15 @@ var startQuizEl = document.querySelector("#start");
 var firstQuestionEl = document.querySelector("#question1");
 var secondQuestionEl = document.querySelector("#question2");
 var thirdQuestionEl = document.querySelector("#question3");
+var fourthQuestionEl = document.querySelector("#question4");
 var timerEl = document.querySelector("#countdown");
+var rightAnswer = document.querySelector("#right");
+var wrongAnswer = document.querySelector("#wrong");
 
 var startQuiz = function(event) {
   //remove start quiz elements
   var startEl = document.getElementById("quizStart");
-  startEl.remove();
+  startEl.style.display = "none";
 
   //create h3 element for question 1
  var firstQuestionEl= document.createElement("h3");
@@ -47,40 +50,30 @@ var answerButtonHandler1 = function(event) {
 
  //display "wrong" for incorrect answer clicked
 if (targetEl.matches('.one, .two, .three')) {
-
-  //create "wrong" banner
-  var wrongAnswer1 = document.createElement("h4");
-  wrongAnswer1.innerText = "WRONG!!";
-  document.getElementById("wrong").appendChild(wrongAnswer1);
+  // var wrongAnswer = document.getElementById("wrong");
+   wrongAnswer.style.display= "block";
 }
-else if (targetEl.matches(".four")) {
-  //create "right" header
-  var rightAnswer1 = document.createElement("h4");
-  rightAnswer1.innerText = "CORRECT!!";
-  document.getElementById("right").appendChild(rightAnswer1);
+else if (targetEl.matches('.four')) {
+  // var rightAnswer = document.getElementById("right");
+   rightAnswer.style.display= "block";
 }
-questionTwo();
+//delay questionTwo function
+setTimeout(questionTwo, 2000);
 };
+
 
 firstQuestionEl.addEventListener("click", answerButtonHandler1);
 
 
-
-
 var questionTwo = function(event) {
-
-  //set delay for funtion
-  var delayQuestion2 = setTimeout(questionTwo, 4000);
-  console.log("delay is working");
-  
-
   //remove first question
-  var removeEl = document.getElementById("question1");
-  removeEl.remove();
-  var wrongAnswer1 = document.getElementById("wrong");
-  wrongAnswer1.remove();
-  var rightAnswer1 = document.getElementById("right");
-  rightAnswer1.remove();
+  var hideEl = document.getElementById("question1");
+  hideEl.style.display = "none";
+  // var wrongAnswer = document.getElementById("wrong");
+  wrongAnswer.style.display = "none";
+  var rightAnswer = document.getElementById("right");
+  rightAnswer.style.display = "none";
+
   //create h3 element for question 2
  var secondQuestionEl= document.createElement("h3");
  secondQuestionEl.className = "quiz";
@@ -108,44 +101,42 @@ var questionTwo = function(event) {
 var answerButtonHandler2 = function(event) {
   //get target element based on answer clicked
   var targetEl = event.target;
-  console.log(targetEl);
  
   //display "wrong" for incorrect answer clicked
- if (targetEl.matches(".one", ".two")) {
- 
-   //create "wrong" banner
-   var wrongAnswer2 = document.createElement("h4");
-   wrongAnswer2.innerText = "WRONG!!";
-   document.getElementById("wrong").appendChild(wrongAnswer2);
- }
- else if (targetEl.matches(".three")) {
-   //create "right" header
-   var rightAnswer2 = document.createElement("h4");
-   rightAnswer2.innerText = "CORRECT!!";
-   document.getElementById("right").appendChild(rightAnswer2);
- }
+ if (targetEl.matches('.one, .two')) {
 
+  //  var wrongAnswer = document.getElementById("wrong");
+   wrongAnswer.style.display= "block";
+ }
+  //display "right" for correct answer
+ else if (targetEl.matches('.three')) {
+  //  var rightAnswer = document.getElementById("right");
+   rightAnswer.style.display= "block";
+ }
+ //delay questionThree function
+ setTimeout(questionThree, 2000)
  };
  
-
 
  secondQuestionEl.addEventListener("click", answerButtonHandler2);
 
 
  var questionThree = function(event) {
-  //remove start quiz elements
-  var removeEl = document.getElementById("question2");
-  removeEl.remove();
-  wrongAnswer2.remove();
-  rightAnswer2.remove();
+   //hide question 2
+  var hideEl = document.getElementById("question2");
+  hideEl.style.display = "none";
+  // var wrongAnswer = document.getElementById("wrong");
+  wrongAnswer.style.display = "none";
+  // var rightAnswer = document.getElementById("right");
+  rightAnswer.style.display = "none";
 
-  //create h3 element for question 1
+  //create h3 element for question 3
  var thirdQuestionEl= document.createElement("h3");
  thirdQuestionEl.className = "quiz";
  thirdQuestionEl.innerText = "What are the three core languages of web pages?";
  document.getElementById("question3").appendChild(thirdQuestionEl);
 
- //create buttons for question 1
+ //create buttons for question 3
  var btn1 = document.createElement("button");
  btn1.innerText = "1. CSS, Python, C#";
  btn1.className ="btn one";
@@ -167,7 +158,81 @@ var answerButtonHandler2 = function(event) {
  document.getElementById("question3").appendChild(btn4);
 };
 
+var answerButtonHandler3 = function(event) {
+  //get target element based on answer clicked
+  var targetEl = event.target;
+ 
+  //display "wrong" for incorrect answer clicked
+ if (targetEl.matches('.one, .two .four')) {
+  //  var wrongAnswer = document.getElementById("wrong");
+   wrongAnswer.style.display= "block";
+ }
+ else if (targetEl.matches('.three')) {
+   var rightAnswer = document.getElementById("right");
+   rightAnswer.style.display= "block";
+ }
+ //delay questionFour function
+ setTimeout(questionFour, 2000)
+ };
 
+thirdQuestionEl.addEventListener("click", answerButtonHandler3);
+
+
+var questionFour = function(event) {
+     //hide question 
+     var hideEl = document.getElementById("question3");
+     hideEl.style.display = "none";
+    //  var wrongAnswer = document.getElementById("wrong");
+     wrongAnswer.style.display = "none";
+     var rightAnswer = document.getElementById("right");
+     rightAnswer.style.display = "none";
+
+  //create h3 elements for question 4
+  var fourthQuestionEl = document.createElement("h3");
+  fourthQuestionEl.className = "quiz";
+  fourthQuestionEl.innerText = "What class of web-development languages are HTML, CSS and JavaScript?";
+  document.getElementById("question4").appendChild(fourthQuestionEl);
+
+  //create buttons for question 4
+  var btn1 = document.createElement("button");
+  btn1.innerText = "1. Front-End Development";
+  btn1.className = "btn one";
+  document.getElementById("question4").appendChild(btn1);
+
+  var btn2 = document.createElement("button");
+  btn2.innerText = "2. Middle-Side Development";
+  btn2.className = "btn two";
+  document.getElementById("question4").appendChild(btn2);
+
+  var btn3 = document.createElement("button");
+  btn3.innerText = "3. Front-Back Development";
+  btn3.className = "btn three";
+  document.getElementById("question4").appendChild(btn3);
+
+  var btn4 = document.createElement("button");
+  btn4.innerText = "4. Back-End Development";
+  btn4.className = "btn four";
+  document.getElementById("question4").appendChild(btn4);
+ 
+};
+
+var answerButtonHandler4 = function(event) {
+  //get target element based on button clicked
+  var targetEl = event.target;
+
+  //display "wrong" for incorrent answer
+  if (targetEl.matches('.two .three .four')) {
+    // var wrongAnswer = document.getElementById("wrong");
+    wrongAnswer.style.display = "block";
+  }
+  //display "correct" for right answer
+  else if (targetEl.matches('.one')) {
+    // var rightAnswer = document.getElementById("right");
+    rightAnswer.style.display = "block";
+  }
+};
+
+fourthQuestionEl.addEventListener("click", answerButtonHandler4);
 
 // var quizTimer = function() {
 
